57263a0d656a4e5e913e921b43ef49fb70a8b3d6
Rowson <root@localhost.localdomain>
2015-05-06 16:52:05 +0800

    This is for practice4

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,9 @@
-#This is app inital file from practice1
-
-#from flask import Flask
-
-#app =Flask(__name__)
-#from app import views
-
-#This is app inital file from practice3
-
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
